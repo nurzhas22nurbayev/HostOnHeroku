11bd2732cb4562f0afc1861289d498076f3d9a67
Nurzhas <montishok@gmail.com>
2021-02-22 03:18:34 +0600

    commit

---
+++ resources/views/welcome2.blade.php
@@ -0,0 +1,3 @@
+<?php
+    echo "Nurbayev Nurzhas";
+?>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where yu can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/wel', function () {
+    return view('welcome2');
+});
+
+Route::get('/test', function () {
+    return "Web programming: back end";
+});
+
+Route::get('/welcome/{id}', function () {
+    return view('welcome3');
+});
